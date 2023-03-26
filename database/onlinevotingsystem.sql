dc8db95cf11c6210f6c889d1517e1ac5319942ee
SHUDDHASHIL21 <74821496+SHUDDHASHIL21@users.noreply.github.com>
2023-03-26 12:40:21 +0530

    first commit

---
+++ database/onlinevotingsystem.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2022 at 03:10 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onlinevotingsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidate_details`
+--
+
+CREATE TABLE `candidate_details` (
+  `id` int(11) NOT NULL,
+  `election_id` int(11) DEFAULT NULL,
+  `candidate_name` varchar(255) DEFAULT NULL,
+  `candidate_details` varchar(45) DEFAULT NULL,
+  `candidate_photo` text DEFAULT NULL,
+  `inserted_by` varchar(255) DEFAULT NULL,
+  `inserted_on` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `candidate_details`
+--
+
+INSERT INTO `candidate_details` (`id`, `election_id`, `candidate_name`, `candidate_details`, `candidate_photo`, `inserted_by`, `inserted_on`) VALUES
+(1, 1, 'Ayushmaan Roy', 'qwerty123', '../assets/images/candidate_photos/90374119594_19302597445eagle.jpg', 'abc', '2022-12-12'),
+(2, 1, 'dert', 'er4t6', '../assets/images/candidate_photos/5503053192_43583322073earth.jpg', 'abc', '2022-12-12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `elections`
+--
+
+CREATE TABLE `elections` (
+  `id` int(11) NOT NULL,
+  `election_topic` varchar(255) DEFAULT NULL,
+  `no_of_candidates` int(11) DEFAULT NULL,
+  `starting_date` date DEFAULT NULL,
+  `ending_date` date DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `inserted_by` varchar(255) DEFAULT NULL,
+  `inserted_on` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `elections`
+--
+
+INSERT INTO `elections` (`id`, `election_topic`, `no_of_candidates`, `starting_date`, `ending_date`, `status`, `inserted_by`, `inserted_on`) VALUES
+(1, 'qwerty', 2, '2022-12-12', '2022-12-13', 'Active', 'abc', '2022-12-12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `contact_no` varchar(45) DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `user_role` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `contact_no`, `password`, `user_role`) VALUES
+(1, 'abc', '123', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Admin'),
+(3, 'test', '234', '0ec09ef9836da03f1add21e3ef607627e687e790', 'Voter'),
+(4, 'Magnolia', '345', '35139ef894b28b73bea022755166a23933c7d9cb', 'Voter');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votings`
+--
+
+CREATE TABLE `votings` (
+  `id` int(11) NOT NULL,
+  `election_id` int(11) DEFAULT NULL,
+  `voters_id` int(11) DEFAULT NULL,
+  `candidate_id` int(11) NOT NULL,
+  `vote_date` date DEFAULT NULL,
+  `vote_time` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `votings`
+--
+
+INSERT INTO `votings` (`id`, `election_id`, `voters_id`, `candidate_id`, `vote_date`, `vote_time`) VALUES
+(1, 1, 3, 1, '2022-12-12', '11:54:41'),
+(2, 1, 4, 1, '2022-12-12', '02:23:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `candidate_details`
+--
+ALTER TABLE `candidate_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `elections`
+--
+ALTER TABLE `elections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `votings`
+--
+ALTER TABLE `votings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `candidate_details`
+--
+ALTER TABLE `candidate_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `elections`
+--
+ALTER TABLE `elections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `votings`
+--
+ALTER TABLE `votings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ voters/inc/footer.php
@@ -0,0 +1,16 @@
+ 
+    <div class="row bg-black text-center text-white">
+        <div class="col-12 my-3">
+            <p> &copy; Copyright 2022 - All Rights Reserved  <br />
+                Developed by Shuddhashil Ganguly - Ayushmaan Roy - Sayantan Maji - Sourish Chaterjee.
+            
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
